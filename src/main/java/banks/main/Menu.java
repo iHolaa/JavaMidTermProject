f90ed542ae1e8aab07dac02e591656e3e7674517
erfan <erfan.fun.af@gmail.com>
2022-04-17 04:04:54 +0430

    main and menu

--- src/main/java/banks/main/Menu.java
+++ src/main/java/banks/main/Menu.java
@@ -1,4 +1,14 @@
 package banks.main;
 
+import java.io.IOException;
+
 public class Menu {
+    Main main = new Main();
+   public void addAdminButton(){
+       try {
+           main.changeScene("AddAdmin.fxml");
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+   }
 }
