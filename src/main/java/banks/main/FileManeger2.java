5412a2a872bc6065316fc9277378a390cb7beed9
erfan <erfan.fun.af@gmail.com>
2022-04-19 06:31:07 +0430

    add admin pages created

---
+++ src/main/java/banks/main/FileManeger2.java
@@ -0,0 +1,32 @@
+package banks.main;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileManeger2 {
+    //Write
+    public void FileWriter(String filePath, String data) {
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+            writer.write(data + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Read
+    public static void FileReader(String fileName, ArrayList<String> data){
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            data.add(br.readLine());
+            br.close();
+
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
