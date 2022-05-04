163cedc23a35663f6346374a01f1e42006cf9d14
erfan <erfan.fun.af@gmail.com>
2022-05-04 18:12:13 +0430

    Finall

--- src/main/java/banks/main/PayBills.java
+++ src/main/java/banks/main/PayBills.java
@@ -1,6 +1,7 @@
 package banks.main;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -8,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class PayBills {
@@ -21,7 +24,7 @@ public class PayBills {
     @FXML
     Text nameText , lastnameText , AmountText ;
     @FXML
-    Button Pay;
+    Button Pay , checkButton ;
     @FXML
     Label errorLabel , successLabel;
     ArrayList<String> HeadBankID = new ArrayList<>();
@@ -33,7 +36,7 @@ public class PayBills {
     ArrayList<String> billAmount = new ArrayList<>();
     ArrayList<String> userCredit = new ArrayList<>();
 
-    int i = 0 , k = 0 , j = 0 , h = 0;
+    int i , k  , j  , h ;
     public void CheckBillButton() {
 
         errorLabel.setText("");
@@ -54,14 +57,9 @@ public class PayBills {
                         LastnameTextField.setVisible(true);
                         NameTextField.setText(name.get(i));
                         LastnameTextField.setText(lastname.get(i));
-                        if(AmountTextField.isVisible() == false) {
-                            AmountTextField.setVisible(true);
-                            AmountTextField.setText(billAmount.get(i));
-                        }
-                        else{
-                            AmountTextField.setVisible(true);
-                            AmountTextField.setText(billAmount.get(i + 1));
-                        }
+                        AmountTextField.setVisible(true);
+                        AmountTextField.setText(billAmount.get(i));
+                        checkButton.setVisible(false);
 
                     } catch (Exception ex) {
                         ex.printStackTrace();
@@ -87,14 +85,10 @@ public class PayBills {
                             LastnameTextField.setVisible(true);
                             NameTextField.setText(name.get(k));
                             LastnameTextField.setText(lastname.get(k));
-                            if(AmountTextField.isVisible() == false) {
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(k));
-                            }
-                            else{
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(k + 1));
-                            }
+                            AmountTextField.setVisible(true);
+                            AmountTextField.setText(billAmount.get(k));
+                            checkButton.setVisible(false);
+
                         } catch (Exception ex) {
                             ex.printStackTrace();
                         }
@@ -119,14 +113,10 @@ public class PayBills {
                             LastnameTextField.setVisible(true);
                             NameTextField.setText(name.get(j));
                             LastnameTextField.setText(lastname.get(j));
-                            if(AmountTextField.isVisible() == false) {
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(j));
-                            }
-                            else{
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(j + 1));
-                            }
+                            AmountTextField.setVisible(true);
+                            AmountTextField.setText(billAmount.get(j));
+                            checkButton.setVisible(false);
+
                         } catch (Exception ex) {
                             ex.printStackTrace();
                         }
@@ -151,15 +141,10 @@ public class PayBills {
                             LastnameTextField.setVisible(true);
                             NameTextField.setText(name.get(h));
                             LastnameTextField.setText(lastname.get(h));
+                            AmountTextField.setVisible(true);
+                            AmountTextField.setText(billAmount.get(j));
+                            checkButton.setVisible(false);
 
-                            if(AmountTextField.isVisible() == false) {
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(j));
-                            }
-                            else{
-                                AmountTextField.setVisible(true);
-                                AmountTextField.setText(billAmount.get(j + 1));
-                            }
                         } catch (Exception ex) {
                             ex.printStackTrace();
                         }
@@ -176,88 +161,117 @@ public class PayBills {
     public void PayBillButton(){
         if(HeadBankCheckBox.isSelected()) {
             try {
-                fm.FileReader("src\\main\\resources\\Files\\DefineBill\\HeadBank\\credit.txt", userCredit);
-                double credit1 = Double.parseDouble(userCredit.get(i));
+                fm.FileReader("src\\main\\resources\\Files\\User\\AddUser\\HeadBank\\AccountCredit.txt", userCredit);
+                double credit1 = Double.parseDouble(userCredit.get(i -1));
                 double billAmount1 = Double.parseDouble(AmountTextField.getText());
-                double originalCredit1 = credit1 - billAmount1;
+                credit1 = credit1 - billAmount1;
 
-                if (originalCredit1 > 20) {
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\HeadBank\\credit.txt", String.valueOf(originalCredit1));
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\HeadBank\\BillAmount.txt", "0");
+                if ( credit1 >= 20) {
+                    userCredit.set( i - 1  , String.valueOf(credit1));
+                    billAmount.set( i - 1 ,"0");
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\User\\AddUser\\HeadBank\\AccountCredit.txt"));
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\DefineBill\\HeadBank\\BillAmount.txt"));
+                    for (int l = 0; l < userCredit.size() ; l++) {
+                        fm.FileWriter("src\\main\\resources\\Files\\User\\AddUser\\HeadBank\\AccountCredit.txt",userCredit.get(l));
+                        fm.FileWriter("src\\main\\resources\\Files\\DefineBill\\HeadBank\\BillAmount.txt",billAmount.get(l));
+                    }
                     AmountTextField.setText("0");
                     successLabel.setText(" Bill Successfully Payed ");
+                    Pay.setVisible(false);
                 } else {
                     errorLabel.setText(" Your Credit is Not Enough !! ");
                 }
             }catch (Exception ex){
                 successLabel.setText(" Return to Menu ");
+                Pay.setVisible(false);
             }
 
-        }
+        }//end of if
         else if(Bank1CheckBox.isSelected()) {
             try {
-                fm.FileReader("src\\main\\resources\\Files\\DefineBill\\Bank1\\credit.txt", userCredit);
-                double credit2 = Double.parseDouble(userCredit.get(k));
-                double billAmount2 = Double.parseDouble(AmountTextField.getText());
-                double originalCredit2 = credit2 - billAmount2;
+                fm.FileReader("src\\main\\resources\\Files\\User\\AddUser\\Bank1\\AccountCredit.txt", userCredit);
+                double credit1 = Double.parseDouble(userCredit.get( k - 1 ));
+                double billAmount1 = Double.parseDouble(AmountTextField.getText());
+                credit1 = credit1 - billAmount1;
 
-                if (originalCredit2 > 20) {
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank1\\credit.txt", String.valueOf(originalCredit2));
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank1\\BillAmount.txt", "0");
+                if ( credit1 >= 20) {
+                    userCredit.set( k - 1 , String.valueOf(credit1));
+                    billAmount.set( k - 1 ,"0");
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\User\\AddUser\\Bank1\\AccountCredit.txt"));
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\DefineBill\\Bank1\\BillAmount.txt"));
+                    for (int l = 0; l < userCredit.size() ; l++) {
+                        fm.FileWriter("src\\main\\resources\\Files\\User\\AddUser\\Bank1\\AccountCredit.txt",userCredit.get(l));
+                        fm.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank1\\BillAmount.txt",billAmount.get(l));
+                    }
                     AmountTextField.setText("0");
                     successLabel.setText(" Bill Successfully Payed ");
+                    Pay.setVisible(false);
                 } else {
                     errorLabel.setText(" Your Credit is Not Enough !! ");
                 }
             }catch (Exception ex){
                 successLabel.setText(" Return to Menu ");
+                Pay.setVisible(false);
             }
 
-        }
+        }//end of else if
         else if(Bank2CheckBox.isSelected()) {
             try {
-                fm.FileReader("src\\main\\resources\\Files\\DefineBill\\Bank2\\credit.txt", userCredit);
-                double credit3 = Double.parseDouble(userCredit.get(j));
-                double billAmount3 = Double.parseDouble(AmountTextField.getText());
-                double originalCredit3 = credit3 - billAmount3;
+                fm.FileReader("src\\main\\resources\\Files\\User\\AddUser\\Bank2\\AccountCredit.txt", userCredit);
+                double credit1 = Double.parseDouble(userCredit.get(j - 1 ));
+                double billAmount1 = Double.parseDouble(AmountTextField.getText());
+                credit1 = credit1 - billAmount1;
 
-                if (originalCredit3 > 20) {
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank2\\credit.txt", String.valueOf(originalCredit3));
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank2\\BillAmount.txt", "0");
+                if ( credit1 >= 20) {
+                    userCredit.set( j - 1 , String.valueOf(credit1));
+                    billAmount.set( j - 1 ,"0");
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\User\\AddUser\\Bank2\\AccountCredit.txt"));
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\DefineBill\\Bank2\\BillAmount.txt"));
+                    for (int l = 0; l < userCredit.size() ; l++) {
+                        fm.FileWriter("src\\main\\resources\\Files\\User\\AddUser\\Bank2\\AccountCredit.txt",userCredit.get(l));
+                        fm.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank2\\BillAmount.txt",billAmount.get(l));
+                    }
                     AmountTextField.setText("0");
                     successLabel.setText(" Bill Successfully Payed ");
-                }
-                else {
+                    Pay.setVisible(false);
+                } else {
                     errorLabel.setText(" Your Credit is Not Enough !! ");
                 }
             }catch (Exception ex){
                 successLabel.setText(" Return to Menu ");
+                Pay.setVisible(false);
             }
-
-        }
-        else if(Bank1CheckBox.isSelected()) {
+        }//end of else if
+        else if(Bank3CheckBox.isSelected()) {
             try {
-                fm.FileReader("src\\main\\resources\\Files\\DefineBill\\Bank3\\credit.txt", userCredit);
-                double credit4 = Double.parseDouble(userCredit.get(h));
-                double billAmount4 = Double.parseDouble(AmountTextField.getText());
-                double originalCredit4 = credit4 - billAmount4;
+                fm.FileReader("src\\main\\resources\\Files\\User\\AddUser\\Bank3\\AccountCredit.txt", userCredit);
+                double credit1 = Double.parseDouble(userCredit.get(h - 1));
+                double billAmount1 = Double.parseDouble(AmountTextField.getText());
+                credit1 = credit1 - billAmount1;
 
-                if (originalCredit4 > 20) {
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank3\\credit.txt", String.valueOf(originalCredit4));
-                    fm2.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank3\\BillAmount.txt", "0");
+                if ( credit1 >= 20) {
+                    userCredit.set( h - 1 , String.valueOf(credit1));
+                    billAmount.set( h - 1 ,"0");
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\User\\AddUser\\Bank3\\AccountCredit.txt"));
+                    Files.deleteIfExists(Path.of("src\\main\\resources\\Files\\DefineBill\\Bank3\\BillAmount.txt"));
+                    for (int l = 0; l < userCredit.size() ; l++) {
+                        fm.FileWriter("src\\main\\resources\\Files\\User\\AddUser\\Bank3\\AccountCredit.txt",userCredit.get(l));
+                        fm.FileWriter("src\\main\\resources\\Files\\DefineBill\\Bank3\\BillAmount.txt",billAmount.get(l));
+                    }
                     AmountTextField.setText("0");
                     successLabel.setText(" Bill Successfully Payed ");
-                }
-                else {
+                    Pay.setVisible(false);
+                } else {
                     errorLabel.setText(" Your Credit is Not Enough !! ");
                 }
             }catch (Exception ex){
                 successLabel.setText(" Return to Menu ");
+                Pay.setVisible(false);
             }
-        }
+        }//end of else if
         else {
             errorLabel.setText(" Select Bank CheckBox !! ");
-        }
+        }//end of else
     }
 
     public void ReturnToMenu(){
